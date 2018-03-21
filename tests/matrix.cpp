7881bb32a11ec3098bdb33a23138d5a575981f03
AlexeyZaharov <32190965+AlexeyZaharov@users.noreply.github.com>
2018-03-21 23:44:38 +0300

    Update matrix.cpp

--- tests/matrix.cpp
+++ tests/matrix.cpp
@@ -31,6 +31,26 @@ TEST_CASE("reading matrix")
     REQUIRE( input == output.str() );
 }
 
+TEST_CASE("reading float_matrix")
+{
+    std::string input{
+        "3, 3\n"
+        "1.1 1.2 1.3\n"
+        "2.4 2.5 2.6\n"
+        "3.7 3.8 3.9" };
+    matrix_t<float> matrix;
+    std::istringstream stream{ input };
+    
+    REQUIRE( matrix.read( stream ) );
+    REQUIRE( matrix.rows() == 3 );
+    REQUIRE( matrix.collumns() == 3 );
+    
+    std::ostringstream output;
+    matrix.write( output );
+    
+    REQUIRE( input == output.str() );
+}
+
 TEST_CASE("addings matrixs")
 {
     std::string first_matrix_representation{
@@ -224,4 +244,27 @@ TEST_CASE("You can`t make this action")
     bool expect = false;
     
     REQUIRE( str_ == expect );
+}
+
+TEST_CASE("experiment_1")
+{
+    std::string first_matrix_representation{
+        "1, 1\n"
+        "1.1\n"
+    };
+    std::string second_matrix_representation{
+        "2, 1\n"
+        "1.0\n"
+        "1.0\n"
+    };
+    
+    std::istringstream stream{ first_matrix_representation };
+    matrix_t<float> first_matrix;
+    first_matrix.read( stream );
+    
+    std::istringstream stream_ { second_matrix_representation };
+    matrix_t<float> second_matrix;
+    second_matrix.read( stream_ );
+    
+    REQUIRE_THROWS_AS(first_matrix.success(second_matrix, *), std::invalid_argument);
 }
