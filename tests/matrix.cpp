bc78e4ae75e0be9ac68125147e0f7e8463c6b8cd
BasyninNikita <32190908+BasyninNikita@users.noreply.github.com>
2018-03-22 22:10:34 +0300

    Update matrix.cpp

--- tests/matrix.cpp
+++ tests/matrix.cpp
@@ -35,7 +35,7 @@ TEST_CASE("reading other type matrix")
         "3, 3\n"
          "1.1 1.2 1.3\n"
          "2.1 2.2 2.3\n"
-         "3.1 3.2 3.3\n"};
+         "3.1 3.2 3.3"};
     matrix_t<float> matrix;
     std::istringstream istream{input};
     REQUIRE( matrix.read(istream));
