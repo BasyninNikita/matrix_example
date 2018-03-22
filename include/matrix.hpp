64897cff05a1fbb7cc2e416681c78d94cc937dc2
BasyninNikita <32190908+BasyninNikita@users.noreply.github.com>
2018-03-23 00:27:18 +0300

    Update matrix.hpp

--- include/matrix.hpp
+++ include/matrix.hpp
@@ -9,16 +9,16 @@ private:
 public:
 	matrix_t() ;
 	matrix_t( matrix_t<T> const & other );
-	matrix_t<T> & operator =( matrix_t<T> const & other );
+	matrix_t & operator =( matrix_t<T> const & other );
 	~matrix_t();
 	std::size_t rows() const;
 	std::size_t collumns() const;
-	matrix_t<T> operator +( matrix_t<T> const & other ) const;
-	matrix_t<T> operator -( matrix_t<T> const & other ) const;
-	matrix_t<T> operator *( matrix_t<T> const & other ) const;
-	matrix_t<T> operator +=( matrix_t<T> const & other ) const;
-	matrix_t<T> operator -=( matrix_t<T> const & other ) const;
-	matrix_t<T> operator *=( matrix_t<T> const & other ) const;
+	matrix_t operator +( matrix_t<T> const & other ) const;
+	matrix_t operator -( matrix_t<T> const & other ) const;
+	matrix_t operator *( matrix_t<T> const & other ) const;
+	matrix_t operator +=( matrix_t<T> const & other ) const;
+	matrix_t operator -=( matrix_t<T> const & other ) const;
+	matrix_t operator *=( matrix_t<T> const & other ) const;
 	std::istream & read( std::istream & stream );
 	std::ostream & write(std::ostream  & stream) const;
 };
